db304565e6153e78308b4b4696616f9a17225298
MaferLeyton <mariferleyton17@gmail.com>
2026-05-20 10:29:35 +0200

    main

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ require_once __DIR__ . '/controlador/inicializador-controlador.php';
     <h3>Calendario Fotografico</h3>
     
     <?php 
-    require_once __DIR__ . '/vista/vista-login.php';
-    require_once __DIR__ . '/vista/vista-calendario.php'; 
-    require_once __DIR__ . '/../controlador/inicializador-controlador.php';
+    require_once __DIR__ . '/vista/inicializador-vista.php';
+
     echo "aqui estoy";
-    
-$listaDeMeses = [1];
-$controlador = new ControlarCalendario($listaDeMeses);
-$mesesIds = $controlador->ObtenerMesesDisponibles();?>
+        
+    $listaDeMeses = [1];
+    $controlador = new ControlarCalendario($listaDeMeses);
+    $mesesIds = $controlador->ObtenerMesesDisponibles();
+    ?>
 </body>
 </html>
---
+++ vista/inicializador-vista.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/vista-login.php';
+require_once __DIR__ . '/vista-calendario.php';
+?>
